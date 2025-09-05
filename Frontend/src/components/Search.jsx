90353f375f9e4e8e87633f5ad56e675499939bc8
Nishant Sharma <nishantsharmanit2023@gmail.com>
2025-09-05 05:52:09 +0530

    Update Search.jsx

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -14,7 +14,7 @@ const Search = () => {
     setError(null)
 
     try {
-      const response = await fetch(`http://localhost:5000/api/user/search?q=${encodeURIComponent(query)}`)
+     const response = await fetch(`https://portfolio-website-backend-stpk.onrender.com/api/user/search?q=${encodeURIComponent(query)}`)
       if (!response.ok) {
         throw new Error('Search failed')
       }
